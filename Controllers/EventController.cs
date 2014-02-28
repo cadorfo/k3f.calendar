2d06f7c734e481c21a4c5f708a0bcdc91f76a5b1
Vitor de Moraes <vitor.np@gmail.com>
2014-02-27 21:35:07 -0300

    Adicionando paginação no controller

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -5,7 +5,6 @@ using System.Web.Mvc;
 using System.Linq;
 using System.Globalization;
 
-
 namespace K3F.Calendar.Controllers
 {
     //[Admin] or [Themed] for a default layout. Can be placed on the controller or methods
@@ -18,18 +17,35 @@ namespace K3F.Calendar.Controllers
             this._repositoryEvent = repositoryEvent;
         }
 
-        public ActionResult ListNextEvents()
+        public ActionResult ListNextEvents(int page)
         {
             var ptBR = new CultureInfo("pt-BR");
             var info = ptBR.DateTimeFormat;
-            var eventos = _repositoryEvent.Fetch(x => x.StartsAt > DateTime.Today).Take(4).Select(x => new
+            var pearPage = 3;
+            var nextPage = (page - 1) * pearPage;
+            var totalEvento = _repositoryEvent.Count(x => x.StartsAt > DateTime.Today);
+            var eventos = _repositoryEvent.Fetch(x => x.StartsAt > DateTime.Today).Skip(nextPage).Take(pearPage).Select(x => new
             {
-                Day = x.StartsAt.Day,
-                WeekDay = info.GetDayName(x.StartsAt.DayOfWeek),
-                Month = info.GetMonthName(x.StartsAt.Month)
+                Name = x.Name,
+                Description = x.Description,
+
+                StartsDay = x.StartsAt.Day,
+                StartsWeekDay = info.GetDayName(x.StartsAt.DayOfWeek),
+                StartsMonth = info.GetMonthName(x.StartsAt.Month),
+
+                EndsDay = x.EndsAt.Day,
+                EndsWeekDay = info.GetDayName(x.EndsAt.DayOfWeek),
+                EndsMonth = info.GetMonthName(x.EndsAt.Month)
             }).ToList();
-            return new JsonResult() { Data = eventos, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            var data = new {
+                PrevPage = nextPage > 0,
+                NextPage = (nextPage + pearPage) <= totalEvento,
+                Data = eventos
+            };
+
+            return new JsonResult() { Data = data, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
         }
 
     }
+
 }
